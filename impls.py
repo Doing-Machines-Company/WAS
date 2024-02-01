34858089ddbc31e7f249df67d35c3c9d11626477
benorgera <benorg@me.com>
2024-01-31 20:59:40 -0600

    Updated requirements, added .gitignore

--- impls.py
+++ impls.py
@@ -12,6 +12,9 @@ import json
 from typing import Optional
 from environment_logger import EnvironmentChange
 
+from dotenv import load_dotenv
+load_dotenv()
+
 api_key = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI(api_key=api_key)
