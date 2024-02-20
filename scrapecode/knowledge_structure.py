24f1947d103f4dcb93b2a4f57af58b95f0166c2b
JamesCXH <70763201+JamesCXH@users.noreply.github.com>
2024-02-20 15:35:11 -0500

    comments

--- scrapecode/knowledge_structure.py
+++ scrapecode/knowledge_structure.py
@@ -1,3 +1,22 @@
+'''
+
+We need a good action space built of symbols
+
+we need to generate this action space via a scrape
+
+every action should be planned using (action, object) types, with action and object as types
+
+on the larger scale, every action should happen on an element of a set of an equivalence class, representing a unique action space class
+
+this action space class has specific ascriptions (like product page for food vs electronics), but have a very similar set of actions ()
+
+we repeat searching for action spaces then performing the planned action on that space, until the set of completed discrete symbol chains is evaluated to be complete
+
+
+
+'''
+
+
 class EquivalenceClass:
     def __init__(self, name, description):
         self.name = name # for human reference only
