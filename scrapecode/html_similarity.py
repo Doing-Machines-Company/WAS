a4e63dbb6900459e54a7670b2bfb9c3c9802fbfa
JamesCXH <70763201+JamesCXH@users.noreply.github.com>
2024-02-23 21:38:35 -0500

    similarity code

---
+++ scrapecode/html_similarity.py
@@ -0,0 +1,114 @@
+from playwright.sync_api import sync_playwright
+import difflib
+from io import StringIO
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+
+def get_classes_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    all_classes = set()
+    for element in soup.find_all(True,
+                                 class_=True):  # True finds all tags, class_=True finds those with a class attribute
+        classes = element.get('class', [])
+        all_classes.update(classes)
+
+    return all_classes
+
+def jaccard_similarity(set1, set2):
+    set1 = set(set1)
+    set2 = set(set2)
+    intersection = len(set1 & set2)
+
+    if len(set1) == 0 and len(set2) == 0:
+        return 1.0
+
+    denominator = len(set1) + len(set2) - intersection
+    return intersection / max(denominator, 0.000001)
+
+def style_similarity(document_1, document_2):
+    """
+    Computes CSS style Similarity between two DOM trees using Playwright
+
+    A = classes(Document_1)
+    B = classes(Document_2)
+
+    style_similarity = |A & B| / (|A| + |B| - |A & B|)
+
+    :param url1: URL of the first page
+    :param url2: URL of the second page
+    :return: Number between 0 and 1 indicating similarity.
+    """
+    classes_page1 = get_classes_from_html(document_1)
+    classes_page2 = get_classes_from_html(document_2)
+    return jaccard_similarity(classes_page1, classes_page2)
+
+
+
+
+class TagExtractor(HTMLParser):
+    """
+    A custom HTML parser designed to extract tags and comments from HTML content.
+    """
+    def __init__(self):
+        super().__init__()
+        self.tags = []
+
+    def handle_starttag(self, tag, attrs):
+        self.tags.append(tag)
+
+    def handle_endtag(self, tag):
+        self.tags.append(tag)
+
+    def handle_comment(self, data):
+        self.tags.append('comment')
+
+def get_tags(html_content):
+    """
+    Extracts and returns all tags and 'comment' for HTML comments from an HTML content.
+
+    :param html_content: HTML content as a string.
+    :return: list of tags and 'comment' strings.
+    """
+    parser = TagExtractor()
+    parser.feed(html_content)
+    return parser.tags
+
+def structural_similarity(document_1, document_2):
+    """
+    Computes the structural similarity between two DOM Trees
+    :param document_1: HTML content as a string
+    :param document_2: HTML content as a string
+    :return: float ratio indicating similarity
+    """
+
+
+    tags1 = get_tags(document_1)
+    tags2 = get_tags(document_2)
+    diff = difflib.SequenceMatcher(None, tags1, tags2)
+    return diff.ratio()
+
+def similarity(document_1, document_2, k=0.6):
+    return k * structural_similarity(document_1, document_2) + (1 - k) * style_similarity(document_1, document_2)
+
+
+url1 = "https://www.amazon.com/REDCON1-Noise-Non-Stim-Preworkout-Watermelon/dp/B08J8BW6KK?ref_=Oct_d_Oct_d_ss_d_6973697011_3&pd_rd_w=bkHf0&content-id=amzn1.sym.73640810-3777-4ff9-82ce-9a53681daf43&pf_rd_p=73640810-3777-4ff9-82ce-9a53681daf43&pf_rd_r=M4F20W3CRMB01GM64PGJ&pd_rd_wg=qxvbl&pd_rd_r=2775b13c-e05a-4ddf-b9c9-b1caa951770b&pd_rd_i=B08J8BW6KK"
+# url2 = "https://www.amazon.com/REDCON1-Noise-Non-Stim-Preworkout-Watermelon/dp/B08J8BW6KK?ref_=Oct_d_Oct_d_ss_d_6973697011_3&pd_rd_w=bkHf0&content-id=amzn1.sym.73640810-3777-4ff9-82ce-9a53681daf43&pf_rd_p=73640810-3777-4ff9-82ce-9a53681daf43&pf_rd_r=M4F20W3CRMB01GM64PGJ&pd_rd_wg=qxvbl&pd_rd_r=2775b13c-e05a-4ddf-b9c9-b1caa951770b&pd_rd_i=B08J8BW6KK"
+url2 = "https://www.amazon.com/gp/product/B002RI97SO?storeType=ebooks&pf_rd_p=114af915-8ac1-4c2e-b2e9-571a645b5906&pf_rd_r=DBVN5590VZV5SS9SEPJ5&pd_rd_wg=NzgDm&pd_rd_i=B002RI97SO&ref_=dbs_f_def_rwt_wigo_cp_recs_wigo_4&pd_rd_w=9tDLW&content-id=amzn1.sym.114af915-8ac1-4c2e-b2e9-571a645b5906&pd_rd_r=037dccd2-8ed5-4fdb-8173-b32710ca17db"
+# url2 = "https://www.amazon.com/live?ref_=nav_cs_amazonlive"
+# url2 = "https://www.amazon.com/Brita-Replacement-BPA-Free-Replaces-Essential/dp/B082TJ4BP6?pd_rd_w=ZNrTH&content-id=amzn1.sym.80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_p=80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_r=04W3NQN7GAKTMN98KCK4&pd_rd_wg=jjnCM&pd_rd_r=2bad8d94-1c69-4a13-ad5f-77ded14e3bee&pd_rd_i=B082TJ4BP6&psc=1&ref_=pd_bap_d_grid_rp_0_10_i"
+# url2 = "https://www.amazon.com/Optimum-Nutrition-Micronized-Monohydrate-Unflavored/dp/B002DYIZEO?pd_rd_w=ZNrTH&content-id=amzn1.sym.80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_p=80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_r=04W3NQN7GAKTMN98KCK4&pd_rd_wg=jjnCM&pd_rd_r=2bad8d94-1c69-4a13-ad5f-77ded14e3bee&pd_rd_i=B002DYIZEO&psc=1&ref_=pd_bap_d_grid_rp_0_4_i"
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(url1)
+    input("wait for load")
+    document_1 = page.content()
+
+    page.goto(url2)
+    input("wait for load")
+    document_2 = page.content()
+
+
+simm = similarity(document_1, document_2)
+
+print(simm)
---
+++ scrapecode/toyscrape.py
@@ -0,0 +1,152 @@
+import asyncio
+from playwright.async_api import async_playwright
+import re
+import json
+from urllib.parse import urlparse, urlunparse
+import os
+from bs4 import BeautifulSoup
+import copy
+
+
+api_key = os.getenv('OPENAI_API_KEY')
+
+
+
+def url_depth(url):
+    parsed = urlparse(url)
+    return parsed.path.count('/')
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    scheme = parsed_url.scheme if parsed_url.scheme else 'http'
+    netloc = parsed_url.netloc
+    path = parsed_url.path.rstrip('/')  # Remove trailing slashes from the path
+    query = parsed_url.query  # Include the query part
+    fragment = parsed_url.fragment  # Include the fragment part
+
+    normalized_url = urlunparse((scheme, netloc, path, '', query, fragment))
+    return normalized_url
+
+
+def aggressive_url_norm(url):  # Very aggressive normalization
+    parsed_url = urlparse(url)
+    scheme = parsed_url.scheme if parsed_url.scheme else 'http'
+    netloc = parsed_url.netloc
+    path = parsed_url.path.rstrip('/')  # Remove trailing slashes from the path
+    # Ignoring the query and fragment
+    normalized_url = urlunparse((scheme, netloc, path, '', '', ''))
+    return normalized_url
+
+
+async def extract_interaction_info(html):  # use general input type
+    if '<a' in html:
+        return "link"
+    if ('<button' in html or "type='button'" in html or "role='button'" in html or "role=\"button\"" in html or "type=\"radio\"" in html or "[onclick]" in html or "onclick=" in html or "[role='button']" in html) and (
+            "<div" not in html):  # filter out div?
+        # or "select" in html DOESN'T HANDLE
+        return "button"
+    if ("<input" in html or "textarea" in html) and "type=\"checkbox\"" not in html and "type=\"radio\"" not in html:
+        # print("HTML HERE!")
+        return "input"
+    if "type=\"checkbox\"" in html:
+        return "checkbox"
+    return "Uncased Element"
+
+
+async def is_good_html(html):  # use general input type
+    if 'hidden' in html:
+        return False
+    if '<a' in html:
+        return False
+    if ('<button' in html or "type='button'" in html or "role='button'" in html or "role=\"button\"" in html or "type=\"radio\"" in html or "[onclick]" in html or "onclick=" in html or "[role='button']" in html) and (
+            "<div" not in html):  # filter out div?
+        # or "select" in html DOESN'T HANDLE
+        return True
+    if ("<input" in html or "textarea" in html) and "type=\"checkbox\"" not in html and "type=\"radio\"" not in html:
+        # print("HTML HERE!")
+        return True
+    if "type=\"checkbox\"" in html:
+        return True
+    return False
+async def get_usable_elements(page):  # maybe remove duplicates if ever needed
+    selector = "a, button, input, select, textarea, [onclick], [role='button']"
+    # await page.wait_for_load_state('networkidle')
+    interactable_elements = await page.locator(selector).element_handles()
+
+
+    return interactable_elements
+
+
+
+
+
+
+
+class IntrastateWebPageNode:
+    unique_interacts_visible = set()
+    unique_interacts_html = set()
+
+    def __init__(self, url=None, acc_tree=None,
+                 embedding=None, actions = None):  # represented by url and action, action taken at url/state
+        self.url = url
+        self.acc_tree = acc_tree
+        self.page_embedding = embedding
+        self.actions = copy.deepcopy(actions)
+
+
+
+
+async def scrape_leaves():
+    async with async_playwright() as p:
+        inner_browser = await p.chromium.launch(headless=False)
+
+        inner_page = await inner_browser.new_page()
+        # for url in ["https://www.amazon.com/CeraVe-Salicylic-Ounce-Fragrance-Exfoliate/dp/B077TWXCQV?pd_rd_w=4Nfcl&content-id=amzn1.sym.80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_p=80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_r=P5SGAK1N1S0YFM9F6Z2V&pd_rd_wg=VSHPH&pd_rd_r=a6092c4d-c5c0-4b92-996c-ca82eb48f8cd&pd_rd_i=B077TWXCQV&psc=1&ref_=pd_bap_d_grid_rp_0_2_i"]:
+        for url in ["https://www.amazon.com/Brita-Standard-Replacement-Pitchers-Dispensers/dp/B00008IHL8?pd_rd_w=vlxeJ&content-id=amzn1.sym.80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_p=80b2efcb-1985-4e3a-b8e5-050c8b58b7cf&pf_rd_r=FPY94C7R438M71B24HG2&pd_rd_wg=HAEGD&pd_rd_r=462b6bc1-aa73-4ab1-bdd3-88172f587bf2&pd_rd_i=B00008IHL8&psc=1&ref_=pd_bap_d_grid_rp_0_7_i"]:
+            await inner_page.goto(url)
+            # await inner_page.wait_for_load_state('networkidle')
+
+            html = await inner_page.content()
+            # print(html)
+            input("take a look")
+
+            usables = await get_usable_elements(inner_page)
+            for element in usables:
+                href = await element.get_attribute('href')
+                html = await element.evaluate("element => element.outerHTML")
+                # if "One-time" in html:
+                #     print(html)
+
+        await inner_page.close()
+        await inner_browser.close()
+
+
+def get_visible_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.get_text(strip=True)
+
+
+def serialize_node(node):
+    """ Serialize the node and its children into a dictionary. """
+    node_data = {
+        'url': node.url,
+        'trajectory': node.actions,
+    }
+    return node_data
+
+
+def save_tree_to_json(root_node, filename):
+    with open(filename, 'w') as file:
+        json.dump(serialize_node(root_node), file, indent=4)
+    print("SAVED TO JSON")
+
+
+async def main():
+    await scrape_leaves()
+
+    # save_tree_to_json(root_node, 'toyamazonscrape.json')
+
+
+asyncio.run(main())
+
