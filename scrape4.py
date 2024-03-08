812a920e94be075e1d6b09fe3d290801e9545a46
JamesCXH <70763201+JamesCXH@users.noreply.github.com>
2024-03-08 02:35:29 -0500

    Update scrape4.py

--- scrape4.py
+++ scrape4.py
@@ -247,9 +247,8 @@ def apply_action(page: PlaywrightPage, a: Action) -> bool:
             return False
 
         case Action.Type.INPUT:
-            print("INPUTTING")
-            print(a.xpath)
-            input_text = a.input_string
+            # input_text = a.input_string
+            input_text = "test"
             try:
                 page.wait_for_load_state('networkidle')
                 page.locator(f'xpath={a.xpath}').first.fill(input_text)
@@ -538,5 +537,5 @@ def explore(starting_url: str, cookies: Optional[dict] = None, headless: bool = 
                 # wait_for_load(page)
         save_equivalence_classes(equiv_classes, output_dir)
 
-explore("https://us.supreme.com/pages/shop", headless=False)
+explore("https://us.supreme.com/pages/shop", headless=True)
 # explore("https://www.amazon.com/Brita-Filter-Pitcher-Standard-Without/dp/B09W4PLVQP/ref=sr_1_7?crid=3LCD2O3C4HNKO&dib=eyJ2IjoiMSJ9.XDFWvhkafbpG8bvke6HUJ1m7eZxOWDVPyhN0MM4tp6A4cF0UNkO2YR9ZtyNOPwzoqrhKHmWWbV5CJxzG_lRfHMy7Vu9fEwo2prr0asnohjrskeR_uMRTyEEIbN3DsS_6Lk-XDjigWxQVxqlDGGkd4MSDIPaU6nltNygG4URYkFf1b5Ib3p_3qlRvmELVRFo3-RxQ95GQVOW1jbYZErMvw5cv0OfHHHobJvcNrc-AgKKc8wXKTyJ4rW4b-FBLokmA23RnUPMO-yC4NJDvodqNabZ-AIbXrRh528W_Y-AwkwY.97zl7k14p0fVKq6Qbr7JmKMcgwchKGD8KgNIznoDwdQ&dib_tag=se&keywords=brita&qid=1709442919&sprefix=brita%2Caps%2C98&sr=8-7&th=1")
